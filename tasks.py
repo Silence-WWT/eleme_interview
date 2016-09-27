a8a3292a0184d6b1367ad51025db74bdff431be0
silencewwt <silencewwt@gmail.com>
2016-09-27 19:06:09 +0800

    update doc

--- tasks.py
+++ tasks.py
@@ -12,19 +12,19 @@ class Producer:
         self.redis = Redis()
         self.app = Flask('task')
         self.db = SQLAlchemy(self.app)
-        self.key = 'lottery'
 
     def produce(self):
         """
         redis.blpop 会从列表开头获取一个元素, 如果列表是空, 则会阻塞
         生产者获取到 Lottery 对象后, 根据该对象的 max_number 生成一定数量的号码, 存入 redis 并更新 max_number
+        因为并发的时候可能会同时多次唤醒生产者, 所以这里需要重新判断是否小于 ALARM_NUM
         """
         with self.app.app_context():
             while True:
-                lottery_id = self.redis.blpop(self.key)[1]
+                lottery_id = self.redis.blpop('lottery')[1]
                 key = 'lottery:%d' % lottery_id
-                if self.redis.llen(key) < ALARM_NUM:
-                    # 通过 self.redis.rpush 推入新生成的号码
+                if self.redis.llen(key) < ALARM_NUM:  # 并发时可能会多次唤醒
+                    # 生成新的号码并更新, 通过 self.redis.rpush 推入新生成的号码, 并更新 max_number
                     pass
 
 
